78197d8645af3ac8a51b4ef97dba6ccaeac69a19
Stefan Pejic <mordnis@mordnis.org>
2021-05-23 14:46:34 +0200

    Fix multiple issues with parsing arguments

--- src/argument_parser.c
+++ src/argument_parser.c
@@ -12,39 +12,48 @@ const char help_option_short[] = "-h";
 
 Arguments ParseArguments(int argc, char* argv[])
 {
-    Arguments args = {NULL, NULL, NULL, 0, true};
+    Arguments args = {NULL, NULL, NULL, 0, false};
 
-    if ((argc == 1) || (strcmp(help_option_short, argv[1]) == 0) || (strcmp(help_option_long, argv[2]) == 0))
+    if (argc < 2)
     {
         args.print_usage_and_quit = true;
         return args;
     }
 
-    int active_index = 1;
+    int idx = 1;
 
-    args.input_file = argv[active_index];
-    ++active_index;
+    if ((argc == 2) && ((strcmp(help_option_short, argv[idx]) == 0) || (strcmp(help_option_long, argv[idx]) == 0)))
+    {
+        args.print_usage_and_quit = true;
+        return args;
+    }
 
-    if (active_index + 1 >= argc)
+    // First argument has to be the input file.
+    args.input_file = argv[idx];
+    idx++;
+
+    // We need at least 2 more arguments to proceed, thus +1.
+    if (idx + 1 >= argc)
     {
         return args;
     }
 
-    if ((strcmp(output_option_short, argv[active_index]) == 0) || (strcmp(output_option_long, argv[active_index]) == 0))
+    if ((strcmp(output_option_short, argv[idx]) == 0) || (strcmp(output_option_long, argv[idx]) == 0))
     {
-        args.output_file = argv[active_index + 1];
-        active_index += 2;
+        args.output_file = argv[idx + 1];
+        idx += 2;
     }
 
-    if (active_index + 1 >= argc)
+    // We need at least 2 more arguments to proceed, thus +1.
+    if (idx + 1 >= argc)
     {
         return args;
     }
 
-    if ((strcmp(args_option_short, argv[active_index]) == 0) || (strcmp(args_option_long, argv[active_index]) == 0))
+    if ((strcmp(args_option_short, argv[idx]) == 0) || (strcmp(args_option_long, argv[idx]) == 0))
     {
-        args.compilation_arguments = (const char* const*)&argv[active_index + 1];
-        args.compilation_argument_count = argc - active_index - 1;
+        args.compilation_arguments = (const char* const*)&argv[idx + 1];
+        args.compilation_argument_count = argc - idx - 1;
     }
     return args;
 }
